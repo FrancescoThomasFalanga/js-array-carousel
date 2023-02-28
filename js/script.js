bba3976d962a844c63d0a5ee420692896d1dcfa8
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-02-28 14:08:13 +0100

    add script

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*
+Dato un array contenente una lista di cinque immagini, 
+creare un carosello come nello screenshot allegato.
+*/
+
+/*
+- preparare un array di path di immagini
+- preparare gli elementi in js corrispondenti agli elementi HTML
+- memorizzare una variabile di indice
+-> alla pressione del pulsante "destra"
+    Aumentare il valore dell'indice di un'unità
+    Mostrare l'immagine alla posizione dell'array relativa al valore dell'indice -1
+    Aggiungi la classe active al cerchio relativo alla posizione dell'indice
+-> alla pressione del pulsante "sinistra"
+    Diminuire il valore dell'indice di un'unità
+    Mostrare l'immagine alla posizione dell'array relativa al valore dell'indice -1
+    Aggiungi la classe active al cerchio relativo alla posizione dell'indice
+*/
